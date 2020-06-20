1d99eeb8d7c681de9d0503820fc04035725a42e2
Deluxe <3017216029@tju.edu.cn>
2020-06-21 01:27:25 +0800

    搭建前后端交互环境

---
+++ src/main/java/com/example/demo/controller/test.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test")
+@CrossOrigin
+public class test {
+    @PostMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+
+}
